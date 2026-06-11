d1182828262c5e09d26dd6fc2add3d99a6954cd0
Binod Borah <40533921+borahbinod@users.noreply.github.com>
2026-06-11 12:51:37 -0600

    Update VisualValidation.R

--- Codes/VisualValidation.R
+++ Codes/VisualValidation.R
@@ -1,11 +1,19 @@
 # to create caterpillar plots to check them
-## analyzing some results
-cov.pars<- c("beta.0","beta.developed_low_intensity","beta.developed_high_intensity",                   # include the quadratic terms if you want to check them too
-             "beta.barren_land","beta.deciduous_forest","beta.evergreen_forest","beta.mixed_forest","beta.shrub_scrub","beta.grassland_herbaceous",
-             "beta.pasture_hay","beta.cultivated_crop","beta.woody_wetland","beta.emergent_wetland",
-             "beta.ocean","beta.lakes","beta.rivers",
-             "beta.elevation", "beta.aspect", "beta.slope",
-             "beta.precipitation","beta.max_temperature", "beta.snow",
+library(MCMCvis)
+### reading individual model output files
+y<-list.files(path="Data_derived/SoE_results",pattern="\\.Rds$",full.names = T) # change path accordingly
+rds_list <- lapply(y, readRDS)
+for (i in 1:length(rds_list)){
+SoE_results<- rds_list[[i]] # reading out species output 
+
+cov.pars<- c("beta.0","beta.developed_low_intensity","beta.developed_low_intensity_sq","beta.developed_high_intensity","beta.developed_high_intensity_sq",
+             "beta.barren_land","beta.barren_land_sq","beta.deciduous_forest","beta.deciduous_forest_sq","beta.evergreen_forest","beta.evergreen_forest_sq","beta.mixed_forest","beta.mixed_forest_sq",
+             "beta.shrub_scrub","beta.shrub_scrub_sq","beta.grassland_herbaceous","beta.grassland_herbaceous_sq",
+             "beta.pasture_hay","beta.pasture_hay_sq","beta.cultivated_crop","beta.cultivated_crop_sq",
+             "beta.woody_wetland","beta.woody_wetland_sq","beta.emergent_wetland","beta.emergent_wetland_sq",
+             "beta.ocean","beta.ocean_sq","beta.lakes","beta.lakes_sq","beta.rivers","beta.rivers_sq",
+             "beta.elevation","beta.elevation_sq", "beta.aspect","beta.aspect_sq", "beta.slope","beta.slope_sq",
+             "beta.precipitation","beta.precipitation_sq","beta.max_temperature","beta.max_temperature_sq", 
              "beta.effort_hours", "beta.number_observers",
              "beta.time_of_day")
 scale.pars<- c("scale.1", "scale.2", "scale.3", "scale.4","scale.5", 
@@ -16,10 +24,12 @@ scale.pars<- c("scale.1", "scale.2", "scale.3", "scale.4","scale.5",
 message(paste0("Model outputs for species: ", SoE_results$scales_results$species[1]))
 message(paste0("presences: ", length(which(SoE_results$data$z==1))," absences: ",length(which(SoE_results$data$z==0)) ))
 SoE_results$start_time; SoE_results$finish_time
+SoE_results$niterations; SoE_results$nthin
 MCMCvis::MCMCtrace(SoE_results$samples, 
-                   params = cov.pars, 
-                   ISB = FALSE, iter=30000,
+                   params = c(cov.pars,scale.pars), # checking three coefficients at a time
+                   ISB = FALSE, iter= 30000,
                    exact = TRUE,
-                   pdf = TRUE)
+                   pdf = TRUE,filename = paste0("Data_derived/Trace_plots/",SoE_results$scales_results$species[1],"_",SoE_results$niterations,"iters","_",SoE_results$nthin,"thin")) # change path accordingly
 
-stats::acf(SoE_results$samples[,"beta.max_temperature"])
+}
+
